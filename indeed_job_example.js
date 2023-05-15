27ae948f1338c745a7b296bf6ef604ed5c8a6823
QuickScraper <app@quickscraper.co>
2023-05-15 16:51:30 +0530

    scrap data of the indeed us

--- indeed_job_example.js
+++ indeed_job_example.js
@@ -5,10 +5,10 @@ const QuickScraperClient = new QuickScraper();
     try {
         const ACCESS_TOKEN = "YLvVRa54d4wZuRwx9DtAy4SYH";
         QuickScraperClient.setAccessToken(ACCESS_TOKEN);
-        const response = await QuickScraperClient.getData("https://in.indeed.com/?r=us&vjk=dc18e8fe5c50c7fb", {
+        const response = await QuickScraperClient.getData("https://www.indeed.com/q-USA-jobs.html", {
           parserSubscriptionId: '9998167e-2d42-5396-9018-0bd35e60e08c'
         });
-        console.log(JSON.stringify(response.data));
+        console.log(JSON.stringify(response.data,null,2));
       } catch (error) {
         console.log("error ", error);
       }
