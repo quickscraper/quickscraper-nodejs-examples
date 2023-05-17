273a9d88573215b65ff5e63fdb6dea5a36237654
QuickScraper <app@quickscraper.co>
2023-05-17 11:37:58 +0530

    update changes for the instagram

--- instagram_profile_example.js
+++ instagram_profile_example.js
@@ -5,7 +5,7 @@ const QuickScraperClient = new QuickScraper();
     try {
         const ACCESS_TOKEN = "YLvVRa54d4wZuRwx9DtAy4SYH";
         QuickScraperClient.setAccessToken(ACCESS_TOKEN);
-        const response = await QuickScraperClient.getData("https://www.instagram.com/khaby00", {
+        const response = await QuickScraperClient.getData("https://instagram.com/cristiano/", {
           parserSubscriptionId: '3e4271e3-e512-5e0f-b1f5-dbe77dc85470'
         });
         console.log(JSON.stringify(response.data));
