0ecfd4f06723db131747a43ca058184391636167
QS Apps <app@quickscraper.co>
2023-09-15 18:53:05 +0530

    added monster search job parser example

--- monster-jobs/monster-jobs-search.js
+++ monster-jobs/monster-jobs-search.js
@@ -1,10 +1,13 @@
 const QuickScraper = require('quickscraper-sdk');
 const QuickScraperClient = new QuickScraper();
+
 (async () => {
     try {
         const ACCESS_TOKEN = "RHQlKDXUgekC2FSvLt3Js7rGh";
         QuickScraperClient.setAccessToken(ACCESS_TOKEN);
-        const response = await QuickScraperClient.getData("https://www.reed.co.uk/jobs/lead-clinical-pharmacist-paediatrics/51204363?source=searchResults&filter=%2Fjobs%2Fhealth-medicine-jobs", { parserSubscriptionId: 'f51e4f06-8079-5ba6-8768-0757d1f02454' });
+        const response = await QuickScraperClient.getData("https://www.monster.co.uk/jobs/q-part-time-jobs?so=p.h.p", {
+            parserSubscriptionId: '706010f0-6d27-5f60-9e98-a7c026555f63'
+        });
         console.log(JSON.stringify(response.data));
     } catch (error) {
         console.log("error ", error);
